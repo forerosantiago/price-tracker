cab0ccca9c032089b700707b98d46c8a69faf4c3
Santiago Forero <saforeros@unal.edu.co>
2025-01-13 23:01:58 -0500

    improve search_exito by using specific selectors for price and name values

--- search_exito.py
+++ search_exito.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 def search_exito(term):
@@ -14,34 +15,43 @@ def search_exito(term):
 
     # find ul element by xpath
     xpath = "/html/body/div[1]/main/section[3]/div/div[2]/div[2]/div[2]/ul"
-    ul_element = driver.find_element(By.XPATH, xpath)
 
+    try:
+        ul_element = driver.find_element(By.XPATH, xpath)
+
+    except NoSuchElementException:
+        driver.quit()
+        return []
     # find all li elements within the ul element
     li_elements = ul_element.find_elements(By.TAG_NAME, "li")
 
     results = []
-
     for li in li_elements:
-        product_info = li.text.split("\n")
-        name = product_info[0]
-        for line in product_info:
-            if line.startswith("$"):
-                price = line
-                # remove $ spaces and . in price
-                price = price.replace("$", "")
-                price = price.replace(" ", "")
-                price = price.replace(".", "")
-                price = float(price)
-                break
+        try:
+            name_element = li.find_element(By.CSS_SELECTOR, "p.styles_name__qQJiK")
+            name = name_element.get_attribute("innerHTML")
+        except NoSuchElementException:
+            name = None
+            break
+
+        try:
+            price_element = li.find_element(
+                By.CSS_SELECTOR, 'p[data-fs-container-price-otros="true"]'
+            )
+            price = price_element.get_attribute("innerHTML")
+            # Remove $ spaces and . in price
+            price = price.replace("$", "").replace(" ", "").replace(".", "")
+            price = float(price)
+
+            print(price)
+        except NoSuchElementException:
+            price = None
+            break
 
         a_element = li.find_element(By.TAG_NAME, "a")
         url = a_element.get_attribute("href")
 
-        result = {
-            "name": name,
-            "price": price,
-            "url": url
-        }
+        result = {"name": name, "price": price, "url": url}
 
         results.append(result)
 
