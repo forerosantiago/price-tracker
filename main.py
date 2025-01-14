799f652b484cb4296dd487cb8a49f5719744840f
Santiago Forero <saforeros@unal.edu.co>
2025-01-13 23:35:55 -0500

    make the set_exito_url method with a simple but interactive interface to search a product and add its url to the Product object

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
 """Module defining an object with a method to fetch prices from various websites."""
+
 import requests
 from bs4 import BeautifulSoup
 
+from prettytable.colortable import ColorTable, Themes
+
+
+from search_exito import search_exito
+
+
 class Product:
     """Class of a prodcut with methods fo fetch prices given URLs"""
-    def __init__(self, name, urls):
+
+    def __init__(self, name, urls=None):
         self.name = name
-        self.urls = urls
+
+        if urls is None:
+            self.urls = []
         self.prices = {}
+
+    def set_exito_url(self, term):
+        """Search for a product on Exito and add the URL to the list of URLs."""
+        results = search_exito(term)
+
+        if not results:
+            print("No results found")
+            return
+        else:
+            print(f"Found {len(results)} results for {term}")
+            table = ColorTable(theme=Themes.OCEAN)
+            table.field_names = ["#", "Name", "Price"]
+
+            for index, result in enumerate(results):
+                table.add_row([index, result["name"], result["price"]])
+                # print(f"URL: {result['url']}")
+
+            print(table)
+
+            input_index = input(
+                "Enter the index of the product you want to add to the list: "
+            )
+            print(results[int(input_index)]["url"])
+
+            self.urls.append(results[int(input_index)]["url"])
 
     def get_prices(self):
         """Fetch prices from the provided URLs and store them in the prices dictionary."""
@@ -15,9 +50,11 @@ class Product:
             # using a regex identify the domain
             domain = url.split("/")[2]
 
-            if (domain == "www.exito.com" or
-                domain == "www.carulla.com" or
-                domain == "www.tiendasjumbo.co"):
+            if (
+                domain == "www.exito.com"
+                or domain == "www.carulla.com"
+                or domain == "www.tiendasjumbo.co"
+            ):
                 r = requests.get(url, timeout=10)
                 if r.status_code == 200:
                     soup = BeautifulSoup(r.content, "html.parser")
@@ -28,3 +65,12 @@ class Product:
                 # agregar makro
             else:
                 print(f"Site {domain} not supported yet")
+
+
+search_term = input("Enter the product you want to search: ")
+test_product = Product(search_term)
+test_product.set_exito_url(search_term)
+
+test_product.get_prices()
+
+print(test_product.prices)
