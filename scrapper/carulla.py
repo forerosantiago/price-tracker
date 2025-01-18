96491255ec7a6645bfe2ed96b2f267e448d416ef
Santiago Forero <saforeros@unal.edu.co>
2025-01-17 20:46:23 -0500

    fix formatting with black linter

--- scrapper/carulla.py
+++ scrapper/carulla.py
@@ -1,8 +1,10 @@
-from scrapper import SharedScrapper
-
 """Definition for CarullaScrapper class."""
+
+from scrapper import SharedScrapper
 
 
 class CarullaScrapper(SharedScrapper):
+    """Class to fetch prices from www.carulla.com"""
+
     def __init__(self):
         super().__init__("www.carulla.com")
--- scrapper/exito.py
+++ scrapper/exito.py
@@ -1,8 +1,10 @@
-from scrapper import SharedScrapper
-
 """Definition for ExitoScrapper class."""
+
+from scrapper import SharedScrapper
 
 
 class ExitoScrapper(SharedScrapper):
+    """Class to fetch prices from www.exito.com"""
+
     def __init__(self):
         super().__init__("www.exito.com")
