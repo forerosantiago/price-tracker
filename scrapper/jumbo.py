4667f31620526ff16d749273b7f2287566e1ec5a
Nigonzalezf <nigonzalezf@unal.edu.co>
2025-01-26 22:43:23 -0500

    Update jumbo.py

--- scrapper/jumbo.py
+++ scrapper/jumbo.py
@@ -1,5 +1,5 @@
 """Definition for JumboScrapper class."""
-#Import necessary libraries.
+# Import necessary libraries.
 import re
 import requests
 from selenium.webdriver.common.by import By
@@ -9,17 +9,16 @@ from scrapper.product import Product
 
 class JumboScrapper(Scrapper):
     """Class to fetch prices from www.jumbo.com"""
-    #Constructor method for the JumboScrapper class.
+    # Constructor method for the JumboScrapper class.
     def __init__(self):
         super().__init__("www.tiendasjumbo.co")
 
     def search(self, term):
         """Get the price of a product from Jumbo."""
         driver = self.get_driver()
-        #Construct the search URL using the provided search term.
         url = f"https://www.tiendasjumbo.co/{term.replace(' ', '%20')}?_q={term.replace(' ', '%20')}&map=ft"
         driver.get(url)
-        #Find all product elements on the page using a CSS selector.
+        # Find all product elements on the page using a CSS selector.
         products = driver.find_elements(
             By.CSS_SELECTOR,
             ".tiendasjumboqaio-cmedia-integration-cencosud-0-x-galleryItem",
@@ -34,7 +33,7 @@ class JumboScrapper(Scrapper):
             price = product.find_element(
                 By.CSS_SELECTOR, ".tiendasjumboqaio-jumbo-minicart-2-x-price"
             )
-            #Convert the price text to a float after cleaning it.
+            # Convert the price text to a float after cleaning it.
             price = float(price.text.replace("$", "").replace(" ", "").replace(".", ""))
             url = product.find_element(By.TAG_NAME, "a")
             result = Product(name.text, url.get_attribute("href"), price)
@@ -43,12 +42,12 @@ class JumboScrapper(Scrapper):
         return results
 
     def get_price(self, url):
-        #Extract the domain from the provided URL and checks if there is a match.
+        # Extract the domain from the provided URL and checks if there is a match.
         domain = url.split("/")[2]
         if domain != self.domain:
             raise RuntimeError("Sitio no soportado")
         r = requests.get(url, timeout=10)
-        #Check if the request was successful (status code 200).
+        # Check if the request was successful (status code 200).
         if r.status_code == 200:
 
 
