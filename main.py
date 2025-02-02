9e2da8f9f5a6d76c545a99f4db0903bfca5c77aa
Santiago Forero <saforeros@unal.edu.co>
2025-02-02 11:22:16 -0500

    refactor main.py to avoid redundancy

--- main.py
+++ main.py
@@ -2,53 +2,33 @@
 
 from concurrent.futures import ThreadPoolExecutor
 from pick import pick
-
 from scrapper.exito import ExitoScrapper
 from scrapper.carulla import CarullaScrapper
 from scrapper.jumbo import JumboScrapper
 
 # Initialize scrapers
-exito = ExitoScrapper()
-carulla = CarullaScrapper()
-jumbo = JumboScrapper()
-
+scrapers = {
+    "Exito": ExitoScrapper(),
+    "Carulla": CarullaScrapper(),
+    "Jumbo": JumboScrapper()
+}
 
 def fetch_products(scraper, search_term):
     """Fetch products from a given scraper."""
     return scraper.search(search_term)
 
-
 # Input from the user
 term = input("Enter your search term: ")
 
-# Create a ThreadPoolExecutor
+# Create a ThreadPoolExecutor and fetch products
 with ThreadPoolExecutor() as executor:
-    # Submit scraper tasks
-    future_exito = executor.submit(fetch_products, exito, term)
-    future_carulla = executor.submit(fetch_products, carulla, term)
-    future_jumbo = executor.submit(fetch_products, jumbo, term)
+    futures = {name: executor.submit(fetch_products, scraper, term) for name, scraper in scrapers.items()}
+    results = {name: future.result() for name, future in futures.items()}
 
-    # Collect results as they finish
-    exito_products = future_exito.result()
-    carulla_products = future_carulla.result()
-    jumbo_products = future_jumbo.result()
-
-if exito_products:
-    option_exito, index_exito = pick(exito_products, "Select a product from Exito: ")
-    print(f"Selected product from Exito: {option_exito}")
-else:
-    print("No products found in Exito")
-
-if carulla_products:
-    option_carulla, index_carulla = pick(
-        carulla_products, "Select a product from Carulla: "
-    )
-    print(f"Selected product from Carulla: {option_carulla}")
-else:
-    print("No products found in Carulla")
-
-if jumbo_products:
-    option_jumbo, index_jumbo = pick(jumbo_products, "Select a product from Jumbo: ")
-    print(f"Selected product from Jumbo: {option_jumbo}")
-else:
-    print("No products found in Jumbo")
+# Process results
+for store, products in results.items():
+    if products:
+        option, _ = pick(products, f"Select a product from {store}: ")
+        print(f"Selected product from {store}: {option}")
+    else:
+        print(f"No products found in {store}")
