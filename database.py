0d129a5985ef06655ab4d4ec180d2153ea5a1435
Santiago Forero <saforeros@unal.edu.co>
2025-02-02 11:56:52 -0500

    add a database and implement basic logic and table layouts

---
+++ database.py
@@ -0,0 +1,53 @@
+"""
+This module sets up the database schema for the price tracker application.
+"""
+
+import sqlite3
+
+def connect_to_db():
+    """Connects to the SQLite database."""
+    return sqlite3.connect('test.db')
+
+def execute_query(query, params=None):
+    """Executes an SQL query."""
+    conn = connect_to_db()
+    cursor = conn.cursor()
+    cursor.execute(query, params or [])
+    conn.commit()
+    conn.close()
+
+execute_query('''
+CREATE TABLE IF NOT EXISTS Products (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+)
+''')
+
+execute_query('''
+CREATE TABLE IF NOT EXISTS Stores (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE,
+    url TEXT UNIQUE
+)
+''')
+
+execute_query('''
+CREATE TABLE IF NOT EXISTS ProductStore (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER REFERENCES Products(id),
+    store_id INTEGER REFERENCES Store(id),
+    name TEXT,
+    url TEXT,
+    last_price REAL,
+    UNIQUE(product_id, store_id)
+)
+''')
+
+execute_query('''
+CREATE TABLE IF NOT EXISTS PriceHistory (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_store_id INTEGER REFERENCES ProductStore(id),
+    price REAL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+''')
--- main.py
+++ main.py
@@ -2,27 +2,55 @@
 
 from concurrent.futures import ThreadPoolExecutor
 from pick import pick
+
 from scrapper.exito import ExitoScrapper
 from scrapper.carulla import CarullaScrapper
 from scrapper.jumbo import JumboScrapper
+
+from database import execute_query
 
 # Initialize scrapers
 scrapers = {
     "Exito": ExitoScrapper(),
     "Carulla": CarullaScrapper(),
-    "Jumbo": JumboScrapper()
+    "Jumbo": JumboScrapper(),
 }
+
+# Create Store entries in the database from srappers with an sql query
+for name, scrapper in scrapers.items():
+    execute_query(
+        """
+    INSERT OR IGNORE INTO Stores (name, url)
+    VALUES (?, ?)
+    """,
+        (name, scrapper.domain),
+    )
+
 
 def fetch_products(scraper, search_term):
     """Fetch products from a given scraper."""
     return scraper.search(search_term)
 
+
 # Input from the user
 term = input("Enter your search term: ")
 
+# Create a product entry in the database from the search term
+execute_query(
+    """
+INSERT INTO Products (name)
+VALUES (?)
+""",
+    (term,),
+)
+
+
 # Create a ThreadPoolExecutor and fetch products
 with ThreadPoolExecutor() as executor:
-    futures = {name: executor.submit(fetch_products, scraper, term) for name, scraper in scrapers.items()}
+    futures = {
+        name: executor.submit(fetch_products, scraper, term)
+        for name, scraper in scrapers.items()
+    }
     results = {name: future.result() for name, future in futures.items()}
 
 # Process results
@@ -30,5 +58,18 @@ for store, products in results.items():
     if products:
         option, _ = pick(products, f"Select a product from {store}: ")
         print(f"Selected product from {store}: {option}")
+
+        # Create a ProductStore entry
+        execute_query(
+            """
+        INSERT INTO ProductStore (product_id, store_id, name, url, last_price)
+        VALUES (
+            (SELECT id FROM Products WHERE name = ?),
+            (SELECT id FROM Stores WHERE name = ?),
+            ?, ?, ?
+        )
+        """,
+            (term, store, option.name, option.url, option.price),
+        )
     else:
         print(f"No products found in {store}")
