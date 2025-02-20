7c86058405093c9e8c520f9761d28459c81f844e
Santiago Forero <saforeros@unal.edu.co>
2025-02-19 20:02:32 -0500

    lint

--- db_setup.py
+++ db_setup.py
@@ -1,27 +1,35 @@
+"""This module initializes the database and creates the necessary tables."""
 import sqlite3
 
+
 def initialize_database():
+    """Initialize the database."""
     with sqlite3.connect("database.db") as conn:
         cursor = conn.cursor()
 
         # Create Stores table
-        cursor.execute("""
+        cursor.execute(
+            """
         CREATE TABLE IF NOT EXISTS Stores (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT UNIQUE,
             url TEXT UNIQUE)
-        """)
-        
+        """
+        )
+
         # Create Products table
-        cursor.execute("""
+        cursor.execute(
+            """
         CREATE TABLE IF NOT EXISTS Products (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT UNIQUE,
             image_url TEXT)
-        """)
+        """
+        )
 
         # Create ProductStores table
-        cursor.execute("""
+        cursor.execute(
+            """
         CREATE TABLE IF NOT EXISTS ProductStores (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             product_id INTEGER REFERENCES Products(id) ON DELETE CASCADE,
@@ -29,32 +37,39 @@ def initialize_database():
             name TEXT,
             url TEXT UNIQUE,
             last_price REAL)
-        """)
+        """
+        )
 
         # Create PriceHistory table
-        cursor.execute("""
+        cursor.execute(
+            """
         CREATE TABLE IF NOT EXISTS PriceHistory (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             product_store_id INTEGER REFERENCES ProductStores(id) ON DELETE CASCADE,
             price REAL,
             date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-        """)
+        """
+        )
 
         # Add supported stores into the Stores table
-        conn.execute("INSERT OR IGNORE INTO Stores (name, url) VALUES (?, ?)",
-            ("Exito", "https://www.exito.com/") 
+        conn.execute(
+            "INSERT OR IGNORE INTO Stores (name, url) VALUES (?, ?)",
+            ("Exito", "https://www.exito.com/"),
         )
 
-        conn.execute("INSERT OR IGNORE INTO Stores (name, url) VALUES (?, ?)",
-            ("Carulla", "https://www.carulla.com/")
+        conn.execute(
+            "INSERT OR IGNORE INTO Stores (name, url) VALUES (?, ?)",
+            ("Carulla", "https://www.carulla.com/"),
         )
 
-        conn.execute("INSERT OR IGNORE INTO Stores (name, url) VALUES (?, ?)",
-            ("Jumbo", "https://www.jumbo.com/")
+        conn.execute(
+            "INSERT OR IGNORE INTO Stores (name, url) VALUES (?, ?)",
+            ("Jumbo", "https://www.jumbo.com/"),
         )
 
         conn.commit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     initialize_database()
     print("Database initialized successfully.")
